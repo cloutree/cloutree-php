28a867792c16ee242046bb2ee6d0488a96c2a421
Ivan Shamatov <status.enable@gmail.com>
2014-09-03 02:10:27 +0400

    initial commit

--- src/Cloutree.php
+++ src/Cloutree.php
@@ -4,18 +4,48 @@ namespace Cloutree;
 
 class Client {
 
-  public static function configure(hash) {
+  // Singleton instance
+  private static $instance; 
 
+  private $app_key;
+  private $app_secret;
+  public $result;
+
+  // private constructor function 
+  // to prevent external instantiation 
+  private __construct() { }
+
+
+  // getInstance method
+  public static function getInstance(){
+    if(!self::$instance) {
+      self::$instance = new self();
+    }
+    return self::$instance;
   }
 
+  // class method to pass to singleton
+  public static function configure(array) {
+    return getInstance->configure(array);
+  }
 
+  // class method to upload files
   public static function upload(filename) {
-
+    return getInstance->upload(filpath);
   }
 
-
+  // class method to get result
   public static function result() {
+    return getInstance->result;
+  }
 
+  private function configure(array) {
+    $this->app_key = array["app_key"];
+    $this->app_secret = array["app_secret"];
+  }
+
+  private function upload(filename) {
+    
   }
 
 }
---
+++ tests/test.php
@@ -0,0 +1,9 @@
+<?
+
+require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+
+use Cloutree\Client;
+
+Client::configure(array("app_key" => "", "app_secret" => ""));
+
+?>
